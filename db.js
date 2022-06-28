176918d4522613c792ae79695aa197eea1618d89
Ameen <ameendv00@gmail.com>
2022-06-28 12:24:16 +0530

    Initial commit

---
+++ db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+module.exports=()=>{
+    const connectionParams={
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+
+    };
+    try {
+        mongoose.connect(process.env.DB,connectionParams)
+        console.log("Database connected")
+    } catch (error) {
+        console.log(error);
+        console.log('Database connection failed');
+    }}
+
